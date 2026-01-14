94b8aea5024175a89688f6c5e0faa91a9739a2bd
mayurpawar17 <mayurpawar4908@gmail.com>
2026-01-15 02:27:44 +0530

    update project img url

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,7 @@ import '../styles/Section.css'
 import '../styles/Projects.css'
 
 import ProjectCard from "../components/ProjectCard";
-import sneakology from '../assets/projectImgs/sneakologBg.jpg'
-import growMate from '../assets/projectImgs/growMateBg.png'
+
 
 const Projects = () => {
   return (
@@ -16,7 +15,7 @@ const Projects = () => {
           title="Sneakology"
           date=""
           desc="Sneakology is a sneaker enthusiast’s paradise—a dedicated platform for sneaker lovers to explore, analyze, and stay updated on the latest hype releases, rare kicks, and streetwear culture."
-          image={sneakology}
+          image='https://github.com/mayurpawar17/published-apps-resources/blob/main/Portfolio/sneakologBg.jpg?raw=true'
           tags={["Website","Github"]}
         />
 
@@ -24,7 +23,7 @@ const Projects = () => {
           title="GrowMate App"
           // date="Mar. 2024 – Mar. 2025"
           desc="GrowMate is an Android application designed to assist plant enthusiasts in managing and taking care of their plants. The app features user authentication via Firebase for a personalized experience."
-         image={growMate}
+         image='https://github.com/mayurpawar17/published-apps-resources/blob/main/Portfolio/growMateBg.png?raw=true'
           tags={["GitHub"]}
         />
 
@@ -32,7 +31,7 @@ const Projects = () => {
           title="Video-Ocean"
           date="Apr. 2025 – Present"
           desc="A video generation platform supporting video, image and audio."
-           image="https://images.unsplash.com/photo-1505238680356-667803448bb6?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+           image="https://github.com/mayurpawar17/published-apps-resources/blob/main/Portfolio/bmi_bg.png?raw=true"
           tags={["Website", "#1 Product Hunt"]}
         />
 
